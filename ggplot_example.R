745fae826267740a3b1e8af329533c6412e3eb1c
cmstouthamer <cmstouthamer@gmail.com>
2015-02-22 15:17:48 -0700

    updated to bar graphs with this melt thing

--- ggplot_example.R
+++ ggplot_example.R
@@ -33,9 +33,38 @@ myplot
 #btw is birthrate
 
 library(MASS)
-ggplot(birthwt, aes(factor(race), bwt)) + geom_boxplot()
+myplot2 <- ggplot(birthwt, aes(factor(race), bwt)) + geom_boxplot()
+myplot2
+summary(myplot2)
 #we say factor(race) because race was entered as a number, but we don't want this
 #to be a continuous variable.
+#The summary variable will tell us what exactly ggplot is doing with each geom, aes etc
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  geom_point(size = 3) +
+  facet_grid(. ~ Species)
+
+#or facet_wrap(~ Species)
+
+#how to make them stacked
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  geom_point(size = 3) +
+  facet_grid(Species ~ .)
+
+df <- melt(iris, id.vars = "Species") #transform a category into a variable
+ggplot(df, aes(Species, value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_brewer(palette = "Set1")
+#This plots the summary statistic (Average? we don't know) for each measure, binned
+#by species. 
+
+
+
+
+
+
+
+
 
 
 
